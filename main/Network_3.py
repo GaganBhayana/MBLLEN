2c324bd85dcee282b2684c9bf16c2002cf27403a
Gagan Bhayana <gagan2oct1998@gmail.com>
2020-05-03 17:35:31 +0530

    Add files via upload

---
+++ main/Network_3.py
@@ -0,0 +1,65 @@
+from keras.layers import Input, Conv2D, Conv2DTranspose, Concatenate, MaxPooling2D, UpSampling2D, Add
+from keras.applications.vgg19 import VGG19
+from keras.models import Model
+from DWT import DWT_Pooling, IWT_UpSampling
+
+
+def down_block(input_layer, filters, kernel_size=(3,3), activation="relu"):
+    # print('-----------')
+    # print(input_layer.shape)
+    output = Conv2D(filters, (3,3), padding="same", activation=activation, data_format='channels_last')(input_layer)
+    # print(output.shape)
+    output = Conv2D(filters, kernel_size, padding="same", activation=activation, data_format='channels_last')(output)
+    # print(output.shape)
+    output = Conv2D(filters*2, kernel_size, padding="same", activation=activation, data_format='channels_last')(output)
+    # print(output.shape)
+    # print('-------------')
+    return output, DWT_Pooling()(output)
+
+def up_block(input_layer, residual_layer, filters, kernel_size=(3,3),activation="relu"):
+    output = IWT_UpSampling()(input_layer)
+    # print(output.shape)
+    output = Add()([residual_layer,output])
+    output = Conv2DTranspose(filters, kernel_size, activation='relu', padding='same', data_format='channels_last')(output)
+    output = Conv2DTranspose(filters, kernel_size, activation='relu', padding='same', data_format='channels_last')(output)
+    output = Conv2DTranspose(filters*2, kernel_size, activation='relu', padding='same', data_format='channels_last')(output)
+    return output
+
+def build_vgg():
+    vgg_model = VGG19(include_top=False, weights='imagenet')
+    vgg_model.trainable = False
+    return Model(inputs=vgg_model.input, outputs=vgg_model.get_layer('block3_conv4').output)
+
+def build_mbllen(input_shape):
+
+    def EM(input, kernel_size, channel):
+        # print(input.shape)
+        down1, pool1 = down_block(input, channel, (kernel_size, kernel_size))
+        # print(down1.shape, pool1.shape)
+        down2, pool2 = down_block(pool1, channel*8,(kernel_size, kernel_size))
+        # print(down2.shape, pool2.shape)
+
+        down3 = Conv2D(channel*32, kernel_size=(3,3), padding="same", activation="relu", data_format='channels_last')(pool2)
+        # print(down3.shape)
+        down4 = Conv2D(channel*64, kernel_size=(3,3), padding="same", activation="relu", data_format='channels_last')(down3)
+        # print(down4.shape)
+
+        up = up_block(down4, down2, channel*4, (kernel_size, kernel_size))#giving half channels since *2 in upblock
+        up = up_block(up, down1, channel, (kernel_size, kernel_size))
+        # print(up.shape)
+        res = Conv2DTranspose(3, (kernel_size,kernel_size), activation='relu', padding='same', data_format='channels_last')(up)
+
+        return res
+
+    inputs = Input(shape=input_shape)
+    FEM = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_last')(inputs)
+    EM_com = EM(FEM, 5, 4)
+
+    for j in range(3):
+        for i in range(0, 3):
+            FEM = Conv2D(32, (3, 3), activation='relu', padding='same', data_format='channels_last')(FEM)
+            EM1 = EM(FEM, 5, 4)
+            EM_com = Concatenate(axis=3)([EM_com, EM1])
+
+    outputs = Conv2D(3, (1, 1), activation='relu', padding='same', data_format='channels_last')(EM_com)
+    return Model(inputs, outputs)
